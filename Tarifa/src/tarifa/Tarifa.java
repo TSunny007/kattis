ae2445854fbcbba15c233f379ec3bad448934906
Tarun Sunkaraneni <tarun.sunkaraneni@gmail.com>
2018-02-17 14:10:07 -0700

    Got my first actual Kattis problem done. Not the last

--- Tarifa/src/tarifa/Tarifa.java
+++ Tarifa/src/tarifa/Tarifa.java
@@ -1,10 +1,23 @@
 package tarifa;
 
+import java.util.Scanner;
+
+// PROBLEM SOLVED : https://open.kattis.com/problems/tarifa
 public class Tarifa {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int bytesPerMonth = 0, months = 0, total = 0;
+		if (sc.hasNextInt()) {
+			bytesPerMonth = sc.nextInt();
+		}
+		if (sc.hasNextInt()) {
+			months = sc.nextInt();
+		} 
+		total = (months+1)*bytesPerMonth;
+		for (int i = 0 ; i < months; i ++) {
+			total-= sc.nextInt();
+		}
+		System.out.println(total);
 	}
 
 }
